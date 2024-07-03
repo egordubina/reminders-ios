2df6ddd68fae5c896e45ec7c21b40bad6ce915b1
Egor Dubina <egordubina@icloud.com>
2024-07-03 13:16:49 +0300

    Initial Commit

---
+++ Todos/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todos
+//
+//  Created by Егор Дубина on 03.07.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
